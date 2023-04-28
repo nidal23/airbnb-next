5ad54747da15ec6923115d4743c286a521aa7edb
Nidal Mohammed <nidalmo23@gmail.com>
2023-04-28 11:57:38 +0530

    modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
@@ -18,7 +19,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={font.className}>
+        <Modal isOpen />
         <Navbar />
         {children}
       </body>
